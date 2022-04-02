4cf89662e4d99d62acde58c8e13ac5b56c0962b0
Joemarcos99 <99109055+Joemarcos99@users.noreply.github.com>
2022-04-02 11:55:36 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+/*
+PFL Score Board
+Author : Joe Marcotte
+Date: 4/2/2022
+*/
+
+
+//Variables
 let teamOneScore = 0;
 let teamTwoScore = 0;
 let scoreBoard = document.getElementById("score");
@@ -9,17 +17,31 @@ const stop = document.getElementById("stop-btn");
 let interval = 0;
 let clicks = 0;
 
+/*
+function called to start timer when start btn is clicked.
+Clicks var used to not make time count down twice as fast
+if clicked twice.
+*/
 start.onclick = function () {
   if (clicks == 0) {
     interval = setInterval(updateTime, 1000);
     clicks = 1;
   }
 };
+
+/*
+function that stops count down
+*/
 stop.onclick = function () {
   clearInterval(interval);
   interval = 0;
   clicks = 0;
 };
+
+/*
+function that decreases time and displays it
+inside the timer label
+*/
 function updateTime() {
   seconds--;
   let min = Math.floor(seconds / 60);
@@ -28,8 +50,13 @@ function updateTime() {
   timer.innerHTML = min + ":" + sec;
 }
 
+/*
+Code for adding functionality to score board and buttons
+*/
+
 scoreBoard.innerHTML = teamOneScore + " - " + teamTwoScore;
 
+//adds action listeners to each btn and adds the value of the btn to the score
 for (let i = 1; i <= 9; i++) {
   let id = "t1Btn" + i;
   document.getElementById(id).onclick = function () {
